1616e16a3a3598cf5ad1e96aabd3e0e5bba789ee
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-01-26 08:40:14 -0300

    feat: implementado requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time and target_time != 0:
+        return None
+    
+    students = 0
+
+    for start, end in permanence_period:
+        if not isinstance(start, int) or not isinstance(end, int):
+            return None
+        if start <= target_time <= end:
+            students += 1
+
+    return students
