12ee49334bc6e106369e63793966fe0b0022e32d
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-01 09:01:16 -0300

    fix: corrigido erro de linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(permanence_period, target_time):
     if not target_time and target_time != 0:
         return None
-    
+
     students = 0
 
     for start, end in permanence_period:
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,16 +1,15 @@
 from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
+
 def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message(1, 2)
-    
+
     with pytest.raises(TypeError):
         encrypt_message("olá", "2")
 
-    
     assert encrypt_message("olá", 6) == "".join(reversed("olá"))
     assert encrypt_message("olá", 2) == "á_lo"
     assert encrypt_message("olá", 2) != "o_lo"
     assert encrypt_message("olá", 2) == "á_lo"
-
