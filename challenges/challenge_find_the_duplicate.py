fff008bc92fff9fb626609003f36cf1da6f0d14a
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-03 12:22:15 -0300

    feat: implementad requisito 5 e 6

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if (not nums or not isinstance(nums[i], int) or nums[i] < 0):
+            return False
+
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
