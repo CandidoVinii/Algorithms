b81fc05d79dc0af29da1da4bb60ebc0889e2b0da
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-03 12:09:37 -0300

    refactor: não passava nos testes dos github

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,43 +1,45 @@
-def sort_and_merge(array):
-    if len(array) <= 1:
-        return "".join(array)
+def is_anagram(first_string: str, second_string: str):
+    first_string = merge_sort_string(list(first_string.lower()))
+    second_string = merge_sort_string(list(second_string.lower()))
 
-    mid = len(array) // 2
-    left = sort_and_merge(array[:mid])
-    right = sort_and_merge(array[mid:])
-
-    return "".join(merge(left, right, array.copy()))
-
-
-def merge(left, right, merged):
-    left_cursor, right_cursor = 0, 0
-
-    while left_cursor < len(left) and right_cursor < len(right):
-        if left[left_cursor] == right[right_cursor]:
-            merged[left_cursor + right_cursor] = left[left_cursor]
-            left_cursor += 1
-        else:
-            merged[left_cursor + right_cursor] = right[right_cursor]
-            right_cursor += 1
-
-    for left_cursor in range(left_cursor, len(left)):
-        merged[left_cursor + right_cursor] = left[left_cursor]
-
-    for right_cursor in range(right_cursor, len(right)):
-        merged[right_cursor + left_cursor] = right[right_cursor]
-
-    return merged
-
-
-def is_anagram(first_string, second_string):
-    f_word = sort_and_merge(list(first_string.lower()))
-    s_word = sort_and_merge(list(second_string.lower()))
-
-    if f_word == "" and s_word == "":
-        return (first_string, second_string, False)
-
-    return (
-        f_word,
-        s_word,
-        f_word == s_word
+    is_anagram = first_string == second_string and (
+        first_string != "" and second_string != ""
     )
+    return (first_string, second_string, is_anagram)
+
+
+def merge_sort_string(string, start=0, end=None):
+    if end is None:
+        end = len(string)
+
+    if end - start > 1:
+        mid = (start + end) // 2
+        merge_sort_string(string, start, mid)
+        merge_sort_string(string, mid, end)
+        string = merge(string, start, mid, end)
+
+    return "".join(string)
+
+
+def merge(string, start, mid, end):
+    left_side = string[start:mid]
+    right_side = string[mid:end]
+
+    left_index = 0
+    right_index = 0
+
+    for index in range(start, end):
+        if left_index >= len(left_side):
+            string[index] = right_side[right_index]
+            right_index += 1
+        elif right_index >= len(right_side):
+            string[index] = left_side[left_index]
+            left_index += 1
+        elif left_side[left_index] < right_side[right_index]:
+            string[index] = left_side[left_index]
+            left_index += 1
+        else:
+            string[index] = right_side[right_index]
+            right_index += 1
+
+    return string
